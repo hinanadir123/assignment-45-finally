8fc43eb72f9ec8a2b3c5b0425a72b216a053fcd0
[hina nadir] <[hinanadirmughal521@gmail.com]>
2024-09-08 12:09:29 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+//assignment no 1
+Object.defineProperty(exports, "__esModule", { value: true });
+//storing the persons name
+let personName = "hina nadir";
+console.log(`hello ${personName} would you like to learn some python today?`);
+//assignment # 2
+//storing a person name in a veriable
+let personname = "Hina nadir";
+console.log("lowercase:", personname.toLowerCase());
+//printing to uppercase
+console.log("uppercase:", personname.toUpperCase());
+//printing to title case
+console.log("Titlecase:", personname.toTitlecase());
+//assignment no 3
+//storing the famous quote and its author
+let quote = "nothing is imposible because impossible itself i am possible";
+let author = "Hina nadir";
+console.log(`"${quote}" - ${author}`);
---
+++ main.ts
@@ -0,0 +1,37 @@
+//assignment no 1
+
+import { log } from "console";
+
+//storing the persons name
+
+let personName: string = "hina nadir"
+console.log(`hello ${personName} would you like to learn some python today?`);
+
+
+//assignment # 2
+
+//storing a person name in a veriable
+
+let personname:any = "Hina nadir"
+console.log("lowercase:", personname.toLowerCase());
+
+//printing to uppercase
+
+console.log("uppercase:", personname. toUpperCase());
+
+//printing to title case
+
+console.log("Titlecase:", personname.replace());
+
+
+//assignment no 3
+
+//storing the famous quote and its author
+
+let quote:string = "nothing is imposible because impossible itself i am possible";
+
+let author:string =  "Hina nadir";
+
+console.log(`"${quote}" - ${author}`);
+
+
